188d8c28928975dd1f30ae574056c881e7997dd4
Chanyang <c81896215@gmail.com>
2024-10-17 13:18:37 +0900

    added comments to color flipper script.js file

--- colorFlipper/script.js
+++ colorFlipper/script.js
@@ -1,11 +1,10 @@
-const body = document.getElementsByTagName('body')[0];
-body.style.backgroundColor = 'red';
+const body = document.getElementsByTagName('body')[0]; // assign variable named body to body tag 
 
 function setColor(name) {
-    body.style.backgroundColor = name;
+    body.style.backgroundColor = name; // body style background color changes to name
 }
 
 function randomColor() {
-    body.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+    body.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`; // randomize hex color code and sets this to background for function randomColor
 }   
 
