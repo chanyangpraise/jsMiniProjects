ee1be73697cb110a887ea5ae8151d0f7de8b518b
Chanyang <c81896215@gmail.com>
2024-10-17 13:22:50 +0900

    added comments to palindromeChecker code

--- palindromeChecker/script.js
+++ palindromeChecker/script.js
@@ -1,14 +1,14 @@
-const input = document.getElementById('input');
+const input = document.getElementById('input'); // create constant variable via input element with id 'input'
 
 function reverseString(str) {
-    return str.split("").reverse().join("")
+    return str.split("").reverse().join("") // create function takes str as perameter which returns str split, reverses it and joins it 
 }
 
 function check() {
-    const value = input.value;
-    const reverse = reverseString(value);
-    if (value == reverse) {
-        alert("P A L I N D R O M E")
+    const value = input.value; // value is current value of an input field
+    const reverse = reverseString(value); // reverse is the reverseString of value
+    if (value == reverse) { // checking if value is same as reverse
+        alert("P A L I N D R O M E") 
     } else {
         alert("Not today!")
     }
